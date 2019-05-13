95b4427a78fe38ba232b336cde6a73c19c9bf362
Eric Chiu <erch1997@gmail.com>
2019-05-13 09:54:57 -0700

    Added code and images for easter egg
    added new images and code for easter egg using mostly jss and changing the css

--- js/easterEgg.js
+++ js/easterEgg.js
@@ -1,11 +1,36 @@
-function easterEgg() {
+/* counter to track number of clicks on a element */
+let count = 0;
 
-	element = document.getElementById("image");
-	element.addEventListener("click", function(e) {
+/* function to increment counter */
+function counter() {
+	count+=1;
+}
+
+/* function for the easter egg */
+function easterEgg(element) {
+	
+	/* When count is under 5, this event listener 
+	refreshes class called easterEgg which shows
+	the shaking animation when clicked */
+	element.addEventListener("click", function a(e) {
 		element.classList.remove("easterEgg");
 		e.preventDefault;
 	    void element.offsetWidth;
 	    element.classList.add("easterEgg");
 	});
 
+	console.log("counter= " + count);
+
+	/* When count is five or greater, the event listener 
+	is overwritten and instead changes the layer clicked
+	into display none */
+	if (count >= 5) {	
+		element.addEventListener("click", function b() {
+			if ( element.style.display == 'none' ) {
+				element.style.display = 'initial';
+			} else {
+				element.style.display = 'none';
+			}
+		});
+	}
 }
